7caf384bce90c1f25fb58f7e7c5824d9af63f3b7
MariaEduardaRufino <maria.rsilva@sptech.school>
2022-12-05 13:20:47 -0300

    Atualizando script e API Arduino

--- Banco de Dados/Script CeresV2.sql
+++ Banco de Dados/Script CeresV2.sql
@@ -1,4 +1,4 @@
--- drop database Ceres;
+ drop database Ceres;
 
 create database Ceres;
 use Ceres;
@@ -13,8 +13,7 @@ idEndereco INT PRIMARY KEY AUTO_INCREMENT,
 estado CHAR(2),
 cidade VARCHAR(65),
 CEP CHAR(8),
-complemento VARCHAR(45),
-numero INT
+complemento VARCHAR(45)
 );
 
 create table Usuario(
@@ -33,6 +32,22 @@ FOREIGN KEY (fkTipo) REFERENCES Tipo(idTipo),
 constraint chkemail check (email like '%@%'),
 primary key (idUsuario)
 );
+
+delimiter $$
+create procedure SP_cadastro_endereco(
+in estado char(2),
+in cidade varchar(45),
+in CEP char(8),
+in complemento varchar(45)
+)
+begin
+	INSERT INTO Endereco (estado, cidade, CEP, complemento) VALUES (estado, cidade, CEP, complemento);
+    select idEndereco from endereco order by idEndereco desc limit 1;
+end $$
+delimiter ;
+
+select * from usuario;
+select * from endereco;
 
 create table Armazem(
 idArmazem int PRIMARY KEY auto_increment,
@@ -81,62 +96,7 @@ INSERT INTO Tipo VALUES
 	(null, 'Produtor Autônomo'),
     (null, 'Empresa');
 
-insert into Usuario (tipo, nome, CNPJ, ddd, telefone, estado, cidade, CEP, complemento, email, username, senha) values
-('Empresa', 'Ceres Seed', '12345678901234', '11', '999999999', 'SP', 'São Paulo' , '01234567', null, 'ceres@seeddashboard.com', 'CERESadmin', '1234');
 
-insert into armazem (tamanho, especie, estado, cidade, CEP, fkuser) values
-(20, 'Café', 'SP','São Paulo','11111111',1),
-(29, 'Café', 'RJ','Rio de Janeiro','22222222',1),
-(8, 'Cacau',  'MG','Extrema','33333333',1),
-(33, 'Araucária', 'SC','Florianópolis','44444444',1),
-(32, 'Café', 'MT','Juína','55555555',1),
-(21, 'Cacau', 'AC','Rio Branco','66666666',1),
-(37, 'Araucária', 'RO','Porto Velho','77777777',1);
-
-insert into Sensor (tipo, fkArmazem) values
-('DHT11', 1), 
-('DHT11', 1), 
-('DHT11', 2), 
-('DHT11', 2), 
-('DHT11', 3), 
-('DHT11', 4), 
-('DHT11', 4), 
-('DHT11', 5), 
-('DHT11', 5), 
-('DHT11', 6), 
-('DHT11', 6), 
-('DHT11', 7), 
-('DHT11', 7), 
-('DHT11', 7);
-
-insert into metrica (idMetrica, temperatura, umidade, fksensor) values
-(1,11,29,10),
-(1,4,74,5),
-(1,2,22,3),
-(1,19,16,7),
-(1,13,26,9),
-(1,14,38,1),
-(1,3,67,8),
-(2,12,47,9),
-(1,9,10,14),
-(3,11,25,9),
-(1,18,59,11),
-(2,8,39,10),
-(1,15,58,12),
-(1,8,42,13),
-(1,19,25,4),
-(2,6,24,14),
-(2,9,99,11),
-(2,6,71,13),
-(1,15,89,2),
-(2,20,44,4),
-(2,11,60,8),
-(1,1,50,6),
-(2,20,93,3),
-(2,18,54,7),
-(2,8,77,5),
-(3,9,63,13),
-(2,5,30,1);
 
 -- SELECT
 
